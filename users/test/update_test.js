18d0963b73a4567de140bdb7bb288b884c14c2fd
tzetzo <tzvetanmarinov@yahoo.com>
2020-06-24 21:37:34 +0300

    update Model Instance i.e. Document using update; extract repetative assertions in helper function;

--- users/test/update_test.js
+++ users/test/update_test.js
@@ -9,15 +9,22 @@ describe("Updating records", () => {
     user.save().then(() => done());
   });
 
-  it("should update Model Instance i.e. Document using set and save", done => {
-    user.set("name", "Alex"); //update property of the document
-    user
-      .save()
+  function assertName(operation, done) {
+    operation
       .then(() => User.find())
       .then(users => {
         assert(users.length === 1);
         assert(users[0].name === "Alex");
         done();
       });
+  }
+
+  it("should update Model Instance i.e. Document using set and save", done => {
+    user.set("name", "Alex"); //update property of the document
+    assertName(user.save(), done);
+  });
+
+  it("should update Model Instance i.e. Document", done => {
+    assertName(user.update({ name: "Alex" }), done);
   });
 });
