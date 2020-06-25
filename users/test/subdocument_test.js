b370ebe88984ca669f895e09a5a1a4eb6eae581e
tzetzo <tzvetanmarinov@yahoo.com>
2020-06-25 11:30:23 +0300

    Add subdocument to existing document

--- users/test/subdocument_test.js
+++ users/test/subdocument_test.js
@@ -2,7 +2,7 @@ const assert = require("assert");
 const User = require("../src/user");
 
 describe("Subdocuments", () => {
-  it("can create a subdocument", (done) => {
+  it("can create a subdocument", done => {
     const user = new User({
       name: "Joe",
       posts: [{ title: "PostTitle" }]
@@ -10,6 +10,26 @@ describe("Subdocuments", () => {
 
     user
       .save()
+      .then(() => User.findOne({ name: "Joe" }))
+      .then(usr => {
+        assert(usr.posts[0].title === "PostTitle");
+        done();
+      });
+  });
+
+  it("can add a subdocument to an existing document", done => {
+    const user = new User({
+      name: "Joe",
+      posts: []
+    });
+
+    user
+      .save()
+      .then(() => User.findOne({ name: "Joe" }))
+      .then(usr => {
+        usr.posts = [...usr.posts, { title: "PostTitle" }];
+        return usr.save();
+      })
       .then(() => User.findOne({ name: "Joe" }))
       .then(usr => {
         assert(usr.posts[0].title === "PostTitle");
